b711896aa6250a7c0e12472be5cbf77ec3f030fb
SSee <samantha.see@vtsd.com>
2024-03-07 12:26:59 -0500

    iteration 2

--- 3.1.3/step3.py
+++ 3.1.3/step3.py
@@ -16,9 +16,49 @@ else:
     print("You chose beef, which will be $6.25")
     subtotal += 6.25
 
-drink = input("Would you like a beverage?")
-drink_size = input("What size drink?")
+drink = input("Would you like a beverage?:")
+drink_size = input("What size drink?:")
 
-if drink == "yes"
-    if drink_size ==
+if drink == "yes":
+    if drink_size == "small":
+        print("You chose small, which will be $1.00")
+        subtotal += 1.00
+    elif drink_size == "medium":
+        print("You chose medium, which will be $1.75")
+        subtotal += 1.75
+    else:
+        print("You chose large, which will be $2.25")
+        subtotal += 2.25
+fries = input("Would you like french fries?:")
+fries_size = input("What size would you like?:")
+if fries == "yes":
+    if fries_size == "small":
+        print("You chose small, which will be $1.00")
+        megasize = input("Would you like to Mega-Size your fries?:")
+        if megasize == "yes":
+            print("You chose to Mega-Size your fries! That will be $2.00")
+            subtotal += 2.00
+        else:
+            subtotal += 1.00
+    elif fries_size == "medium":
+        print("You chose medium, which will be $1.50")
+        subtotal += 1.50
+    else:
+        print("You chose large, which will be $2.00")
+        subtotal += 2.00
+
+ketchup = input(int("How many ketchup packets would you like?"))
+
+
+if drink == "yes":
+    if fries == "yes":
+        if ketchup > 0:
+            subtotal -= 1.00
+
+
+
+
+
+
+print(subtotal)
 
