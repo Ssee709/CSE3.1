20d0656a3a3e3dff81b3a17bbd0110625965a427
SSee <samantha.see@vtsd.com>
2024-03-19 13:10:41 -0400

    3.2.2

---
+++ 3.2.2/main.py
@@ -0,0 +1,17 @@
+from Post import Post
+
+all_posts_archive = []
+'''
+# your code here
+post1 = Post("Marie", "This is my first post!")
+
+post2 = Post("Bob", "Hello")
+
+post3 = Post("Gerald", "Posted")
+
+print(post1)
+print(post2)
+print(post3)
+'''
+
+username = input("What would you like your username to be?: ")
